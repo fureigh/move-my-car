371e5f69cd328277a8e97f6d30ec60f2d3e33040
Fureigh <info@fureigh.com>
2013-12-14 21:00:59 -0500

    Refactored to use and work with the 311Today RSS feed.

--- alternate-side-parking.html.php
+++ alternate-side-parking.html.php
@@ -12,58 +12,70 @@
     <?php
       date_default_timezone_set('America/New_York');
 
-      $filename = 'http://www.nyc.gov/html/dot/downloads/misc/' . date('Y') . '_alternate_side.ics';
+      $filename = 'http://www1.nyc.gov/apps/311/311Today.rss';
       $file_headers = @get_headers($filename);
 
       if (($file_headers[0] != 'HTTP/1.0 404 Not Found') && ($file_headers[0] != 'HTTP/1.0 302 Found')) {
-        $off_days = file_get_contents($filename);
+        $asp_data = file_get_contents($filename);
       }
 
       $date_string = get_date_string();
-      if ($date_string && $off_days) {
-        $needle = 'DTSTART;VALUE=DATE:' . $date_string;
-      }
-      else {
+
+      if (!($date_string && $asp_data)) {
         $the_verdict = 'Whoops.';
-        $description = "There's a problem with the date. Bug Fureigh about it, will you?";
-      }
-
-      // Intentionally assigning a value to $start.
-      if ($start = strpos($needle, $off_days)) {
-        // Yes, alternate side parking is suspended tomorrow.
-        // So no, you don't need to move your car.
-        $the_verdict = 'No';
-
-        // Search for the next instance of 'DESCRIPTION.'
-        $remainder = substr($off_days, $start);
-        $description = substr($remainder, strpos('DESCRIPTION:', $remainder));
-        $description = ltrim($description, 'DESCRIPTION:');
+        $description = "There's a problem with the date. Bug <a href=\"http://www.fureigh.com\">Fureigh</a> about it, will you?";
       }
       else {
-        // Sorry, bub, alternate side parking rules are in effect tomorrow.
-        $the_verdict = 'Yes';
-        $description = 'You gotta move your car before tomorrow. Or <a href="http://transalt.org" target="_blank">switch to a bike</a>.';
+        // Assignment operator is intentional.
+        if ($tomorrow = strpos($asp_data, $date_string)) {
+          // Assignment operator is intentional.
+          // @todo: Find out what time the RSS feed updates. If tomorrow were ever listed as the first day (i.e., today), 
+          // you could get a false "not in effect" if alternate side parking rules were to be not in effect the day after tomorrow.
+
+          $needle = 'Alternate side parking not in effect';
+
+          if ($not_in_effect = strpos($asp_data, $needle, $tomorrow)) {
+            // Yes, alternate side parking is suspended tomorrow.
+            // So no, you don't need to move your car.
+            $the_verdict = 'No';
+
+            // @todo: See whether 311 provides a description or whether you can only get it from NYC Open Data's iCal file.
+            $description = 'You don\'t have to move your car before tomorrow. Lucky you.';
+          }
+          else {
+            // Sorry, bub, alternate side parking rules are in effect tomorrow.
+            $the_verdict = 'Yes';
+            $description = 'You gotta move your car before tomorrow. Or <a href="http://transalt.org" target="_blank">switch to a bike</a>.';
+          }
+        }
+        // @todo: Create a set_error function. Call it above and here too.
       }
 
-      print $the_verdict . '!';
+      // It sure should exist, but being on the safe side...
+      if (!is_null($the_verdict)) {
+        print $the_verdict . '!';
+      }
 
       function get_date_string() {
         // If today's the last day of the month,
-        if (date('d') == date('t')) {
+        if (date('j') == date('t')) {
           // Tomorrow's the first day of the next month.
-          $month = date('m');
+          $month = date('F');
 
           // If this is the last day of December, tomorrow's a new year too.
-          if ($month == '12') {
+          if ($month == 'December') {
             $year = date('Y') + 1;
           }
           else {
             $year = date('Y');
           }
-          $date_string  = $year . $month + 1 . '01';
+          $date_string  = 'January 1, ' . $year;
         }
         else {
-          $date_string  = date('Ym') . date('d') + 1;
+          // Saturday, December 15, 2013
+          // l, F j, Y. But we don't actually need the day of the week; searching for the day, month, year will suffice.
+          // @todo: Find out whether the 311 uses leading zeros for days. If so, change j to d.
+          $date_string  = date('F') . ' ' . (date('j') + 1) . ', ' . date('Y');
         }
 
         if ($date_string) {
